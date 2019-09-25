f717ec17ee94021a2731bcf848f73bf8b7f1932f
sftth <sftth318@gmail.com>
2019-09-25 17:29:21 +0900

    Jacoco 설정

--- src/test/java/com/summit/springboot/template/TemplateApplicationTests.java
+++ src/test/java/com/summit/springboot/template/TemplateApplicationTests.java
@@ -1,9 +1,12 @@
 package com.summit.springboot.template;
 
+import com.summit.springboot.template.login.LoginController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +14,12 @@ public class TemplateApplicationTests {
 
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void loginTest() {
+        LoginController loginController = new LoginController();
+        assertEquals("Hello Travic CI", loginController.init());
     }
 
 }
